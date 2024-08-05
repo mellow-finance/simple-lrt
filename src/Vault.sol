6c0e2b0111b22971a05779cecfbc93237ee23c6b
Alex Euler <0xalexeuler@gmail.com>
2024-08-05 17:01:17 +0300

    added docs

--- src/Vault.sol
+++ src/Vault.sol
@@ -20,9 +20,22 @@ contract Vault is ERC20, AccessControlEnumerable {
 
     using SafeERC20 for IERC20;
 
-    // Limit in wstETH
+    // @notice The maximum amount of LRT that can be minted
     uint256 limit;
 
+    /**
+     * @notice The constructor
+     *
+     * @param name The name of the LRT token
+     * @param ticker The ticker of the LRT token
+     * @param admin The address of the admin
+     *
+     * @dev Admin should be a timelock contract
+     *
+     * @custom:effects
+     * - Sets the `name` and `ticker` state variables for ERC20
+     * - Grants the DEFAULT_ADMIN_ROLE to the `admin` param
+     */
     constructor(
         string memory name,
         string memory ticker,
@@ -31,7 +44,19 @@ contract Vault is ERC20, AccessControlEnumerable {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
-    // Set under openzeppelin timelock
+    /**
+     * @notice Set the limit of LRT that can be minted
+     *
+     * @param _limit The new limit
+     *
+     *
+     * @custom:requirements
+     * - MUST have admin role to set limit
+     *
+     * @custom:effects
+     * - Updates `limit` state variable
+     * - Emits NewLimit event
+     */
     function setLimit(uint256 _limit) external {
         require(
             hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
@@ -41,6 +66,32 @@ contract Vault is ERC20, AccessControlEnumerable {
         emit NewLimit(_limit);
     }
 
+    /**
+     * @notice Deposit either of wstETH / stETH / wETH / ETH into the vault.
+     *
+     * @param token The address of the token to deposit.
+     *              0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE is used for raw ETH and
+     *              ether is sent directly as a value in this case
+     * @param amount The amount of the token to deposit. In case of raw ETH it should
+     *               be equal to `msg.value`
+     * @param recipient The address of the recipient of the LRT
+     * @param referral The address of the referral
+     *
+     * @custom:requirements
+     * - The amount of the token to deposit MUST be greater than 0
+     * - The token sent MUST be either of wstETH / stETH / wETH / ETH
+     * - If raw ETH is sent then `token` MUST be 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
+     * - If raw ETH is sent `amount` MUST be equal to `msg.value`
+     * - If anything other than raw ETH is sent `msg.value` MUST be 0
+     * - If anything other than raw ETH is sent the sender MUST ERC20-approve at least `amount`
+     *   to this contract
+     *
+     * @custom:effects
+     * - Transfers the `token` with `amount` from the sender to the vault
+     * - Mints the `amount` of LRT to the `recipient`
+     * - Calls `push()` to transfer the wstETH to the Symbiotic default bond
+     * - Emits Deposit event
+     */
     function deposit(
         address token,
         uint256 amount,
@@ -64,7 +115,16 @@ contract Vault is ERC20, AccessControlEnumerable {
         emit Withdrawal(msg.sender, amount);
     }
 
-    // Push everything to the bond
+    /**
+     *@notice Pushes all wstETH from the vault balance to the Symbiotic default bond (up to its limit)
+     *
+     *@dev This function is called after a deposit to the vault and can be called by
+     *     any external address to ensure that the wstETH is earning yield.
+     *
+     *@custom:effects
+     * - calls IDefaultBond#deposit with the vault's balance of wstETH (up to the limit of the bond)
+     * - Emits Push event
+     */
     function push() public {
         uint256 amount = IERC20(wstETH).balanceOf(address(this));
         amount = _trimToLimit(DEFAULT_BOND, amount);
@@ -73,8 +133,12 @@ contract Vault is ERC20, AccessControlEnumerable {
         }
         IERC20(wstETH).safeIncreaseAllowance(DEFAULT_BOND, amount);
         IDefaultBond(DEFAULT_BOND).deposit(address(this), amount);
+        emit Push(amount);
     }
 
+    /**
+     * @dev Internal function to transfer the token to the vault and convert it to wstETH
+     */
     function _transferToVaultAndConvertToWsteth(
         address token,
         uint256 amount,
@@ -84,6 +148,7 @@ contract Vault is ERC20, AccessControlEnumerable {
             revert("Vault: amount must be greater than 0");
         }
         if (token != ETH) {
+            require(msg.value == 0, "Vault: cannot send ETH with token");
             IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
         }
         if (token == WETH) {
@@ -91,6 +156,7 @@ contract Vault is ERC20, AccessControlEnumerable {
             token = ETH;
         }
         if (token == ETH) {
+            require(msg.value == amount, "Vault: incorrect amount of ETH");
             ISTETH(stETH).submit{value: amount}(referral);
             token = stETH;
         }
@@ -105,6 +171,9 @@ contract Vault is ERC20, AccessControlEnumerable {
         return amount;
     }
 
+    /**
+     * @dev Internal function to trim amounts for both Vault and Symbiotic Default Bond limits
+     */
     function _trimToLimit(
         address vault,
         uint256 amount
@@ -116,4 +185,5 @@ contract Vault is ERC20, AccessControlEnumerable {
     event Deposit(address indexed user, uint256 amount, address referral);
     event Withdrawal(address indexed user, uint256 amount);
     event NewLimit(uint256 limit);
+    event Push(uint256 amount);
 }
