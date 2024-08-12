fe27491c67dcafa846eb5e7dfb1293be7b0b2046
Alex Euler <0xalexeuler@gmail.com>
2024-08-12 10:30:52 +0300

    0.8.26 bump

--- src/BaseVault.sol
+++ src/BaseVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSL-1.1
-pragma solidity 0.8.25;
+pragma solidity 0.8.26;
 
 import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 
