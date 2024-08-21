55149955db2c6d373aaf318965ee6bca07478e4c
Anton Iants <yants.anton@gmail.com>
2024-08-21 17:11:39 +0400

    [eigen] start implementation

---
+++ src/MellowEigenLayerVault.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.25;
+
+import {ERC4626Vault} from "./ERC4626Vault.sol";
+import {VaultControl, VaultControlStorage} from "./VaultControl.sol";
+import "./interfaces/vaults/IMellowEigenLayerVault.sol";
+
+contract MellowEigenLayerVault is
+    IMellowEigenLayerVault,
+    ERC4626Vault
+{
+    using SafeERC20 for IERC20;
+    using Math for uint256;
+
+    address public strategy;
+
+    constructor(bytes32 contractName_, uint256 contractVersion_)
+        VaultControlStorage(contractName_, contractVersion_)
+    {}
+
+    function initialize(InitParams memory initParams) public virtual initializer {
+        __initialize(initParams);
+    }
+
+    function __initialize(InitParams memory initParams) internal virtual onlyInitializing {
+        // EIP-1271 signature
+        bytes memory signature;
+        ISignatureUtils.SignatureWithExpiry memory signatureWithExpiry = ISignatureUtils.SignatureWithExpiry(signature, block.timestamp + 365 days);
+        bytes32 salt;
+        IDelegationManager(initParams.delegationManager).delegateTo(initParams.operator, signatureWithExpiry, salt);
+        
+        strategy = initParams.strategy;
+        address underlyingToken = address(IStrategy(strategy).underlyingToken());
+
+        __initializeERC4626(
+            initParams.admin,
+            initParams.limit,
+            initParams.depositPause,
+            initParams.withdrawalPause,
+            initParams.depositWhitelist,
+            underlyingToken,
+            initParams.name,
+            initParams.symbol
+        );
+    }
+
+    // ERC4626 overrides
+    function totalAssets()
+        public
+        view
+        virtual
+        override(ERC4626Upgradeable, IERC4626)
+        returns (uint256)
+    {
+        return IERC20(asset()).balanceOf(address(this)); // TODO check
+    }
+
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares)
+        internal
+        virtual
+        override
+    {
+        super._deposit(caller, receiver, assets, shares);
+        uint256 actualShares = IStrategy(strategy).deposit(IERC20(asset()), assets);
+        require(actualShares >= shares, "Vault: insufficient shares");
+    }
+
+    /**
+     * @dev Internal conversion function (from assets to shares) with support for rounding direction.
+     */
+    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view override returns (uint256 shares) {
+
+        shares = IStrategy(strategy).underlyingToSharesView(assets);
+
+        if (shares > 0 && rounding == Math.Rounding.Floor) {
+            shares -= 1;
+        } else if (rounding == Math.Rounding.Ceil) {
+            shares += 1;
+        }
+    }
+
+    /**
+     * @dev Internal conversion function (from shares to assets) with support for rounding direction.
+     */
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view override returns (uint256 assets) {
+
+        assets = IStrategy(strategy).sharesToUnderlyingView(shares);
+
+        if (assets > 0 && rounding == Math.Rounding.Floor) {
+            assets -= 1;
+        } else if (rounding == Math.Rounding.Ceil) {
+            assets += 1;
+        }
+    }
+
+    /// @notice Internal function used to fetch this contract's current balance of `underlyingToken`.
+    // slither-disable-next-line dead-code
+    function _tokenBalance() internal view virtual returns (uint256) {
+        return IERC20(asset()).balanceOf(strategy);
+    }
+}
---
+++ src/interfaces/vaults/IMellowEigenLayerVault.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.25;
+
+import {IERC4626Vault} from "./IERC4626Vault.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {
+    ERC4626Upgradeable,
+    IERC4626
+} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+
+import "../eigen/IStrategyManager.sol";
+import "../eigen/IDelegationManager.sol";
+
+interface IMellowEigenLayerVault is IERC4626Vault {
+    struct InitParams {
+        uint256 limit;
+        address strategyManager;
+        address delegationManager;
+        address strategy;
+        address operator;
+        address admin;
+        bool depositPause;
+        bool withdrawalPause;
+        bool depositWhitelist;
+        string name;
+        string symbol;
+    }
+
+    function initialize(InitParams memory initParams) external;
+}
