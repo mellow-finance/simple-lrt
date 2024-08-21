296c50ada763759a3bd5ecb9ffae3573ffafd02d
Andrey Borzenkov <armoking32@gmail.com>
2024-08-21 21:20:50 +0400

    fixes: EthVaultCompat - ERC20 -> ERC20Upgradeable migration logic

--- src/EthVaultCompat.sol
+++ src/EthVaultCompat.sol
@@ -5,6 +5,14 @@ import "./interfaces/vaults/IEthVaultCompat.sol";
 
 import {MellowSymbioticVault} from "./MellowSymbioticVault.sol";
 
+/*
+    This contract is an intermediate step in the migration from mellow-lrt/src/Vault.sol to simple-lrt/src/MellowSymbioticVault.sol.
+    Migration logic:
+
+    1. On every transfer/mint/burn, the _update function is called, which transfers the user's balance from the old storage slot to the new one.
+    2. At the same time, the old _totalSupply decreases. This allows tracking how many balances still need to be migrated.
+    3. Once the old _totalSupply reaches zero, further migration to MellowSymbioticVault can be performed. This will remove unnecessary checks.
+*/
 contract EthVaultCompat is IEthVaultCompat, MellowSymbioticVault {
     // ERC20 slots
     mapping(address account => uint256) private _balances;
@@ -12,41 +20,41 @@ contract EthVaultCompat is IEthVaultCompat, MellowSymbioticVault {
 
     uint256 private _totalSupply;
 
-    string private _name;
-    string private _symbol;
-
     constructor() MellowSymbioticVault("EthVaultV1", 1) {}
+    
+    // decreases with migrations
+    // when it becomes zero -> we can migrate to MellowSymbioticVault
+    function compatTotalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function migrateMultiple(address[] calldata users) external {
+        for (uint256 i = 0; i < users.length;) {
+            migrate(users[i]);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    // helps migrate user balance from default ERC20 stores to ERC20Upgradeable stores
+    function migrate(address user) public {
+        uint256 balance = _balances[user];
+        if (balance == 0) {
+            return;
+        }
+        delete _balances[user];
+        unchecked {
+            _totalSupply -= balance;
+        }
+        _mint(user, balance);
+    }
 
     // ERC20Upgradeable override
-
     function _update(address from, address to, uint256 value) internal virtual override {
-        if (from == address(0)) {
-            // Overflow check required: The rest of the code assumes that totalSupply never overflows
-            _totalSupply += value;
-        } else {
-            uint256 fromBalance = _balances[from];
-            if (fromBalance < value) {
-                revert ERC20InsufficientBalance(from, fromBalance, value);
-            }
-            unchecked {
-                // Overflow not possible: value <= fromBalance <= totalSupply.
-                _balances[from] = fromBalance - value;
-            }
-        }
-
-        if (to == address(0)) {
-            unchecked {
-                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
-                _totalSupply -= value;
-            }
-        } else {
-            unchecked {
-                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
-                _balances[to] += value;
-            }
-        }
-
-        emit Transfer(from, to, value);
+        migrate(from);
+        migrate(to);
+        super._update(from, to, value);
     }
 
     function balanceOf(address account)
@@ -55,54 +63,10 @@ contract EthVaultCompat is IEthVaultCompat, MellowSymbioticVault {
         override(IERC20, ERC20Upgradeable)
         returns (uint256)
     {
-        return _balances[account];
-    }
-
-    function allowance(address owner, address spender)
-        public
-        view
-        override(IERC20, ERC20Upgradeable)
-        returns (uint256)
-    {
-        return _allowances[owner][spender];
-    }
-
-    function _approve(address owner, address spender, uint256 value, bool emitEvent)
-        internal
-        virtual
-        override
-    {
-        if (owner == address(0)) {
-            revert ERC20InvalidApprover(address(0));
-        }
-        if (spender == address(0)) {
-            revert ERC20InvalidSpender(address(0));
-        }
-        _allowances[owner][spender] = value;
-        if (emitEvent) {
-            emit Approval(owner, spender, value);
-        }
+        return _balances[account] + super.balanceOf(account);
     }
 
     function totalSupply() public view override(IERC20, ERC20Upgradeable) returns (uint256) {
-        return _totalSupply;
-    }
-
-    function symbol()
-        public
-        view
-        override(IERC20Metadata, ERC20Upgradeable)
-        returns (string memory)
-    {
-        return _symbol;
-    }
-
-    function name()
-        public
-        view
-        override(IERC20Metadata, ERC20Upgradeable)
-        returns (string memory)
-    {
-        return _name;
+        return _totalSupply + super.totalSupply();
     }
 }
