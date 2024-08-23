5c1291184467e3711f91d6821e199f030a54fd5f
Andrey Borzenkov <armoking32@gmail.com>
2024-08-23 17:31:09 +0400

    solvency init

---
+++ test/solvency/SolvencyTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.25;
+
+import "../BaseTest.sol";
+
+contract SolvencyTest is BaseTest {
+    /*
+        1. functions for random values
+        2. transitions: deposit, withdraw, claim, slash, param changes, e.t.c, rewards, push rewards, push into symbiotic
+        3. validation funciton. totalAssets <= limit, ...
+        4. finalization
+        5. final_validation 
+    */
+
+    function testSolvency() external {
+        /*
+            1. deploy
+
+        */
+    }
+}
