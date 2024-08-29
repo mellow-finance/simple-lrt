9f79bd74e3145f28137d3b3aabeb788d756920bb
maxim <max.kvant@gmail.com>
2024-08-29 13:20:07 +0300

    deploy implemented

--- test/solvency/SolvencyTest.t.sol
+++ test/solvency/SolvencyTest.t.sol
@@ -11,6 +11,27 @@ contract SolvencyTest is BaseTest {
         4. finalization
         5. final_validation 
     */
+    address admin = makeAddr("admin");
+    address user = makeAddr("user");
+
+    uint256 private seed;
+
+    uint256 public constant MAX_ERROR = 10 wei;
+    uint256 public constant Q96 = 2 ** 96;
+    uint256 public constant D18 = 1e18;
+
+    address wsteth = 0x8d09a4502Cc8Cf1547aD300E066060D043f6982D;
+    address steth = 0x3F1c547b21f65e10480dE3ad8E19fAAC46C95034;
+    address weth = 0x94373a4919B3240D86eA41593D5eBa789FEF3848;
+    
+    address limitIncreaser = makeAddr("limitIncreaser");
+
+    uint64 vaultVersion = 1;
+    address vaultOwner = makeAddr("vaultOwner");
+    address vaultAdmin = makeAddr("vaultAdmin");
+    uint48 epochDuration = 3600;
+
+    uint256 symbioticLimit = 1000 ether;
 
     function testSolvency() external {
         /*
@@ -19,5 +40,115 @@ contract SolvencyTest is BaseTest {
             3. finalization
             4. final validation
         */
+
+        seed = 0;
+
+        MellowSymbioticVault singleton = new MellowSymbioticVault("MellowSymbioticVault", 1);
+        MellowSymbioticVaultFactory factory = new MellowSymbioticVaultFactory(address(singleton));
+
+        ISymbioticVault symbioticVault = ISymbioticVault(
+            symbioticHelper.createNewSymbioticVault(
+                SymbioticHelper.CreationParams({
+                    vaultOwner: vaultOwner,
+                    vaultAdmin: vaultAdmin,
+                    epochDuration: epochDuration,
+                    asset: wsteth,
+                    isDepositLimit: false,
+                    depositLimit: symbioticLimit
+                })
+            )
+        );
+
+        (IMellowSymbioticVault mellowSymbioticVault1, IWithdrawalQueue withdrawalQueue1) = factory
+            .create(
+            IMellowSymbioticVaultFactory.InitParams({
+                proxyAdmin: makeAddr("proxyAdmin"),
+                limit: 100 ether,
+                symbioticVault: address(symbioticVault),
+                admin: admin,
+                depositPause: false,
+                withdrawalPause: false,
+                depositWhitelist: false,
+                name: "MellowSymbioticVault",
+                symbol: "MSV"
+            })
+
+
+        );
+
+        vm.startPrank(user);
+
+        vm.stopPrank();
     }
+
+    function transition_random_deposit() internal {
+        
+    }
+
+    function transition_random_wsteth_price_change() internal {
+        
+    }
+
+    function transition_request_random_withdrawal() internal {
+        
+    }
+
+    function transition_process_random_requested_withdrawals_subset() internal {
+        
+    }
+
+    function transfer_rogue_deposit() internal {
+        
+    }
+
+    function finilizeTest() internal {
+
+    }
+
+    function _random() internal returns (uint256) {
+        seed = uint256(
+            keccak256(abi.encodePacked(block.timestamp, block.prevrandao, seed))
+        );
+        return seed;
+    }
+
+    function _randInt(uint256 maxValue) internal returns (uint256) {
+        return _random() % (maxValue + 1);
+    }
+
+    function _randInt(
+        uint256 minValue,
+        uint256 maxValue
+    ) internal returns (uint256) {
+        return (_random() % (maxValue - minValue + 1)) + minValue;
+    }
+
+    function random_float_x96(
+        uint256 minValue,
+        uint256 maxValue
+    ) internal returns (uint256) {
+        return _randInt(minValue * Q96, maxValue * Q96);
+    }
+
+    function random_bool() internal returns (bool) {
+        return _random() & 1 == 1;
+    }
+
+    function random_address() internal returns (address) {
+        return address(uint160(_random()));
+    }
+
+    function calc_random_amount_d18() internal returns (uint256 result) {
+        uint256 result_x96 = random_float_x96(D18, 10 * D18);
+        if (random_bool()) {
+            uint256 b_x96 = random_float_x96(1e0, 1e6);
+            result = Math.mulDiv(result_x96, b_x96, Q96) / Q96;
+            assertLe(1 ether, result, "amount overflow");
+        } else {
+            uint256 b_x96 = random_float_x96(1e1, 1e10);
+            result = Math.mulDiv(result_x96, Q96, b_x96) / Q96;
+            assertGe(1 ether, result, "amount underflow");
+        }
+    }
+
 }
