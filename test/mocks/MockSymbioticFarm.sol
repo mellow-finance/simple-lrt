d5c23c5525674b9bbebb7691696afc7197694428
Andrey Borzenkov <armoking32@gmail.com>
2024-09-05 14:13:08 +0400

    unit tests: WIP

---
+++ test/mocks/MockSymbioticFarm.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.25;
+
+import "../Imports.sol";
+
+contract MockSymbioticFarm is IStakerRewards {
+    function version() external pure returns (uint64) {
+        return 1;
+    }
+
+    function claimable(address, address, bytes calldata) external pure returns (uint256) {
+        return 0;
+    }
+
+    function distributeRewards(address network, address token, uint256 amount, bytes calldata data)
+        external
+    {}
+
+    function claimRewards(address recipient, address token, bytes calldata /* data */ ) external {
+        IERC20(token).transfer(recipient, IERC20(token).balanceOf(address(this)));
+    }
+
+    function test() private pure {}
+}
