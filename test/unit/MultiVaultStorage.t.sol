61d49ce0c5824b1fbdd70894ab0eac2eaaf23022
Andrey Borzenkov <armoking32@gmail.com>
2024-12-11 20:30:13 +0400

    tests: MultiVaultStorage 100%

--- test/unit/MultiVaultStorage.t.sol
+++ test/unit/MultiVaultStorage.t.sol
@@ -71,4 +71,79 @@ contract Unit is BaseTest {
         vm.expectRevert("MultiVaultStorage: subvault already exists");
         c.addSubvault(address(8), address(9), IMultiVaultStorage.Protocol.ERC4626);
     }
+
+    function testRemoveSubvault() external {
+        MockMultiVaultStorage c = new MockMultiVaultStorage("test", 1);
+        assertNotEq(address(c), address(0));
+
+        c.initializeMultiVaultStorage(
+            address(1), address(2), address(3), address(4), address(5), address(6), address(7)
+        );
+        c.addSubvault(address(8), address(9), IMultiVaultStorage.Protocol.ERC4626);
+        c.addSubvault(address(10), address(11), IMultiVaultStorage.Protocol.EIGEN_LAYER);
+
+        assertEq(c.subvaultsCount(), 2);
+
+        vm.expectRevert("MultiVaultStorage: subvault already exists");
+        c.addSubvault(address(8), address(9), IMultiVaultStorage.Protocol.ERC4626);
+
+        c.removeSubvault(address(8));
+        assertEq(c.subvaultsCount(), 1);
+        assertEq(c.indexOfSubvault(address(8)), 0);
+        assertEq(c.indexOfSubvault(address(10)), 1);
+
+        vm.expectRevert("MultiVaultStorage: subvault not found");
+        c.removeSubvault(address(8));
+
+        c.removeSubvault(address(10));
+        assertEq(c.subvaultsCount(), 0);
+    }
+
+    function testSetRewardsData() external {
+        MockMultiVaultStorage c = new MockMultiVaultStorage("test", 1);
+        assertNotEq(address(c), address(0));
+
+        c.initializeMultiVaultStorage(
+            address(1), address(2), address(3), address(4), address(5), address(6), address(7)
+        );
+
+        IMultiVaultStorage.RewardData memory rewardData;
+
+        c.setRewardData(0, rewardData);
+        assertEq(c.farmCount(), 0);
+
+        rewardData.token = address(12);
+        c.setRewardData(0, rewardData);
+        assertEq(c.farmCount(), 1);
+        assertEq(c.farmIdAt(0), 0);
+        vm.expectRevert();
+        c.farmIdAt(1);
+
+        rewardData.token = address(13);
+        c.setRewardData(0, rewardData);
+        assertEq(c.farmCount(), 1);
+        assertEq(c.farmIdAt(0), 0);
+        vm.expectRevert();
+        c.farmIdAt(1);
+
+        rewardData.token = address(12);
+        c.setRewardData(1, rewardData);
+        assertEq(c.farmCount(), 2);
+        assertEq(c.farmIdAt(0), 0);
+        assertEq(c.farmIdAt(1), 1);
+        vm.expectRevert();
+        c.farmIdAt(2);
+
+        rewardData.token = address(0);
+        c.setRewardData(0, rewardData);
+        assertEq(c.farmCount(), 1);
+        assertEq(c.farmIdAt(0), 1);
+        vm.expectRevert();
+        c.farmIdAt(1);
+
+        c.setRewardData(1, rewardData);
+        assertEq(c.farmCount(), 0);
+        vm.expectRevert();
+        c.farmIdAt(0);
+    }
 }
