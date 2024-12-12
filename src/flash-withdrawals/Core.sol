64e214510e2245654108fe04db6d4c9f6c03eee3
Andrey Borzenkov <armoking32@gmail.com>
2024-12-12 14:38:50 +0400

    Flash withdrawals: init

---
+++ src/flash-withdrawals/Core.sol
@@ -0,0 +1,67 @@
+// // SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "../vaults/MultiVault.sol";
+import "./Instance.sol";
+
+contract Core {
+    using SafeERC20 for IERC20;
+
+    uint256 public constant D18 = 1 ether;
+
+    address public immutable mm;
+    address public immutable multiVault;
+    address public immutable asset;
+    address public immutable claimer;
+
+    uint256 public withdrawalDelay;
+    uint256 public instantRateD18;
+    uint256 public feeD18;
+    uint256 public minVolume;
+
+    modifier onlyMM() {
+        require(msg.sender == mm, "Core: forbidden");
+        _;
+    }
+
+    constructor(address _mm, address _multiVault, address claimer_) {
+        mm = _mm;
+        multiVault = _multiVault;
+        asset = MultiVault(multiVault).asset();
+        claimer = claimer_;
+    }
+
+    function setWithdrawalDelay(uint256 delay) external onlyMM {
+        withdrawalDelay = delay;
+    }
+
+    function allocateFunds(uint256 amount) external onlyMM {
+        IERC20(asset).transferFrom(mm, address(this), amount);
+    }
+
+    function setRate(uint256 minVolume_, uint256 instantRateD18_, uint256 feeD18_)
+        external
+        onlyMM
+    {
+        minVolume = minVolume_;
+        instantRateD18 = instantRateD18_;
+        feeD18 = feeD18_;
+    }
+
+    function createRequest(uint256 lpAmount, uint256 minInstant, uint256 maxFee, address recipient)
+        external
+        returns (Instance instance)
+    {
+        uint256 assets = MultiVault(multiVault).previewRedeem(lpAmount);
+        uint256 instant = Math.min(
+            Math.mulDiv(assets, instantRateD18, D18), IERC20(asset).balanceOf(address(this))
+        );
+        require(instant >= minInstant, "Core: insufficient instant funds");
+        uint256 fee = Math.mulDiv(assets - instant, feeD18, D18);
+        require(fee <= maxFee, "Core: fee exceeds limit");
+
+        instance = new Instance(recipient, claimer, instant + fee);
+        MultiVault(multiVault).redeem(lpAmount, address(instance), msg.sender);
+        IERC20(asset).safeTransfer(recipient, instant);
+    }
+}
---
+++ src/flash-withdrawals/Instance.sol
@@ -0,0 +1,44 @@
+// // SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "../utils/Claimer.sol";
+import "./Core.sol";
+
+contract Instance {
+    using SafeERC20 for IERC20;
+
+    uint256 public constant D18 = 1 ether;
+
+    address public immutable recipient;
+    Core public immutable core;
+    Claimer public immutable claimer;
+
+    uint256 public mmAssets;
+
+    constructor(address recipient_, address claimer_, uint256 mmAssets_) {
+        recipient = recipient_;
+        core = Core(msg.sender);
+        claimer = Claimer(claimer_);
+        mmAssets = mmAssets_;
+    }
+
+    function claimWithdrawals(
+        uint256[] calldata subvaultIndices,
+        uint256[][] calldata indices,
+        uint256 maxAssets
+    ) external {
+        uint256 assets = claimer.multiAcceptAndClaim(
+            core.multiVault(), subvaultIndices, indices, address(this), maxAssets
+        );
+
+        uint256 mmAssets_ = Math.min(mmAssets, assets);
+        if (mmAssets_ != 0) {
+            assets -= mmAssets;
+            mmAssets -= mmAssets_;
+            IERC20(core.asset()).safeTransfer(core.mm(), mmAssets_);
+        }
+        if (assets != 0) {
+            IERC20(core.asset()).safeTransfer(recipient, assets);
+        }
+    }
+}
