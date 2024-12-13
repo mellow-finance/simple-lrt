294daeed5f0dd348f2dd0a7f6493a89da5617ef3
Andrey Borzenkov <armoking32@gmail.com>
2024-12-13 04:32:03 +0400

    tests: Symbiotic 100%

--- test/unit/SymbioticAdapter.t.sol
+++ test/unit/SymbioticAdapter.t.sol
@@ -2,5 +2,38 @@
 pragma solidity 0.8.25;
 
 import "../BaseTest.sol";
+import "../mocks/MockSymbioticFarm.sol";
 
-contract Unit is BaseTest {}
+contract Unit is BaseTest {
+    using RandomLib for RandomLib.Storage;
+
+    function testSymbioticAdapter() external {
+        MultiVault vault = new MultiVault("test", 1);
+        Claimer claimer = new Claimer();
+        SymbioticAdapter symbioticAdapter = new SymbioticAdapter(address(vault), address(claimer));
+
+        vm.expectRevert("SymbioticAdapter: delegate call only");
+        symbioticAdapter.pushRewards(address(0), new bytes(0), new bytes(0));
+
+        vm.expectRevert("SymbioticAdapter: delegate call only");
+        symbioticAdapter.withdraw(address(0), address(0), address(0), 0, address(0));
+
+        vm.expectRevert("SymbioticAdapter: delegate call only");
+        symbioticAdapter.deposit(address(0), 0);
+
+        vm.expectRevert("SymbioticAdapter: only vault");
+        symbioticAdapter.handleVault(address(0));
+
+        (address symbioticVault,,, address admin) =
+            symbioticHelper.createDefaultSymbioticVault(Constants.WSTETH());
+        vm.prank(admin);
+        ISymbioticVault(symbioticVault).setDepositWhitelist(true);
+
+        assertEq(0, symbioticAdapter.maxDeposit(symbioticVault));
+
+        vm.prank(admin);
+        ISymbioticVault(symbioticVault).setDepositorWhitelistStatus(address(vault), true);
+
+        assertEq(type(uint256).max, symbioticAdapter.maxDeposit(symbioticVault));
+    }
+}
