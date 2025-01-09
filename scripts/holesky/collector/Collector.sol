e21c8dbc0c94202c7d1e3b70768a133f7b23ffea
Andrey Borzenkov <armoking32@gmail.com>
2025-01-09 21:29:53 +0400

    Collector: WIP

---
+++ scripts/holesky/collector/Collector.sol
@@ -0,0 +1,378 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "../../../src/interfaces/queues/IEigenLayerWithdrawalQueue.sol";
+import "../../../src/interfaces/queues/ISymbioticWithdrawalQueue.sol";
+
+import "../../../src/interfaces/tokens/IWSTETH.sol";
+import "../../../src/vaults/MultiVault.sol";
+import "./Oracle.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract Collector {
+    struct Withdrawal {
+        uint256 subvaultIndex;
+        uint256 assets;
+        bool isTimestamp;
+        uint256 claimingTime; // if 0 - assets == claimable assets, otherwise = pending assets
+    }
+
+    struct Response {
+        address vault;
+        address asset;
+        uint8 assetDecimals;
+        uint256 assetPriceX96;
+        uint256 totalLP;
+        uint256 totalUSD;
+        uint256 totalETH;
+        uint256 totalUnderlying;
+        uint256 limitLP;
+        uint256 limitUSD;
+        uint256 limitETH;
+        uint256 limitUnderlying;
+        uint256 userLP;
+        uint256 userETH;
+        uint256 userUSD;
+        uint256 userUnderlying;
+        uint256 lpPriceUSD;
+        uint256 lpPriceETH;
+        uint256 lpPriceUnderlying;
+        Withdrawal[] withdrawals;
+    }
+
+    struct FetchDepositAmountsResponse {
+        bool isDepositPossible;
+        bool isDepositorWhitelisted;
+        uint256[] ratiosD18; // multiplied by 1e18 for weis of underlying tokens
+        address[] tokens;
+        uint256 expectedLpAmount; // in lp weis 1e18
+        uint256 expectedLpAmountUSDC; // in USDC weis 1e8 (due to chainlink decimals)
+        uint256[] expectedAmounts; // in underlying tokens weis
+        uint256[] expectedAmountsUSDC; // in USDC weis 1e8 (due to chainlink decimals)
+    }
+
+    uint256 private constant Q96 = 2 ** 96;
+    uint256 private constant D9 = 1e9;
+    uint256 private constant D18 = 1e18;
+
+    address public constant eth = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public immutable usd = address(bytes20(keccak256("usd-token-address")));
+    address public immutable wsteth;
+    address public immutable weth;
+    address public immutable steth;
+    address public owner;
+    Oracle public oracle;
+
+    mapping(address vault => bool) public isDVV;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Collector: not owner");
+        _;
+    }
+
+    constructor(address wsteth_, address weth_, address steth_, address owner_) {
+        wsteth = wsteth_;
+        weth = weth_;
+        steth = steth_;
+        owner = owner_;
+    }
+
+    function setOwner(address owner_) external onlyOwner {
+        owner = owner_;
+    }
+
+    function setOracle(address oracle_) external onlyOwner {
+        oracle = Oracle(oracle_);
+    }
+
+    function setIsDVV(address vault, bool flag) external onlyOwner {
+        isDVV[vault] = flag;
+    }
+
+    function collect(address user, IERC4626 vault_) public view returns (Response memory r) {
+        MultiVault vault = MultiVault(address(vault_));
+
+        r.vault = address(vault_);
+        r.asset = vault_.asset();
+        r.assetDecimals = IERC20Metadata(r.asset).decimals();
+        r.assetPriceX96 = oracle.priceX96(r.asset);
+
+        r.totalLP = vault_.totalSupply();
+        r.totalUnderlying = vault_.totalAssets();
+        r.totalETH = oracle.getValue(r.asset, r.totalUnderlying);
+        r.totalUSD = oracle.getValue(r.asset, usd, r.totalUnderlying);
+
+        r.limitLP = vault.limit();
+        r.limitUnderlying = vault.convertToAssets(r.limitLP);
+        r.limitETH = oracle.getValue(r.asset, r.limitUnderlying);
+        r.limitUSD = oracle.getValue(r.asset, usd, r.limitUnderlying);
+
+        r.userLP = vault.balanceOf(user);
+        r.userUnderlying = vault.convertToAssets(r.userLP);
+        r.userETH = oracle.getValue(r.asset, r.userUnderlying);
+        r.userUSD = oracle.getValue(r.asset, usd, r.userUnderlying);
+
+        r.lpPriceUSD = Math.mulDiv(1 ether, r.totalUSD, r.totalLP);
+        r.lpPriceETH = Math.mulDiv(1 ether, r.totalETH, r.totalLP);
+        r.lpPriceUnderlying = Math.mulDiv(1 ether, r.totalUnderlying, r.totalLP);
+
+        Withdrawal[] memory withdrawals = new Withdrawal[](vault.subvaultsCount() * 50);
+        uint256 iterator = 0;
+        uint256 subvaultsCount = vault.subvaultsCount();
+        IMultiVaultStorage.Subvault memory subvault;
+        for (uint256 subvaultIndex = 0; subvaultIndex < subvaultsCount; subvaultIndex++) {
+            subvault = vault.subvaultAt(subvaultIndex);
+            if (subvault.withdrawalQueue == address(0)) {
+                continue;
+            }
+
+            IWithdrawalQueue queue = IWithdrawalQueue(subvault.withdrawalQueue);
+
+            {
+                uint256 claimable = queue.claimableAssetsOf(user);
+                if (claimable != 0) {
+                    withdrawals[iterator++] = Withdrawal({
+                        subvaultIndex: subvaultIndex,
+                        assets: claimable,
+                        isTimestamp: false,
+                        claimingTime: 0
+                    });
+                }
+            }
+
+            if (queue.pendingAssetsOf(user) == 0) {
+                continue;
+            }
+
+            if (subvault.protocol == IMultiVaultStorage.Protocol.SYMBIOTIC) {
+                ISymbioticWithdrawalQueue q = ISymbioticWithdrawalQueue(subvault.withdrawalQueue);
+                (uint256 sharesToClaimPrev, uint256 sharesToClaim,,) = q.getAccountData(user);
+                ISymbioticVault symbioticVault = q.symbioticVault();
+                uint256 currentEpoch = q.getCurrentEpoch();
+
+                if (sharesToClaimPrev != 0) {
+                    ISymbioticWithdrawalQueue.EpochData memory epochData =
+                        q.getEpochData(currentEpoch);
+
+                    if (!epochData.isClaimed) {
+                        uint256 assets = Math.mulDiv(
+                            symbioticVault.withdrawalsOf(currentEpoch - 1, address(q)),
+                            sharesToClaimPrev,
+                            epochData.sharesToClaim
+                        );
+                        if (assets != 0) {
+                            withdrawals[iterator++] = Withdrawal({
+                                subvaultIndex: subvaultIndex,
+                                assets: assets,
+                                isTimestamp: true,
+                                claimingTime: symbioticVault.currentEpochStart()
+                                    + symbioticVault.epochDuration()
+                            });
+                        }
+                    }
+                }
+                if (sharesToClaim != 0) {
+                    ISymbioticWithdrawalQueue.EpochData memory epochData =
+                        q.getEpochData(currentEpoch + 1);
+                    if (!epochData.isClaimed) {
+                        uint256 assets = Math.mulDiv(
+                            symbioticVault.withdrawalsOf(currentEpoch + 1, address(q)),
+                            sharesToClaim,
+                            epochData.sharesToClaim
+                        );
+                        if (assets != 0) {
+                            withdrawals[iterator++] = Withdrawal({
+                                subvaultIndex: subvaultIndex,
+                                assets: assets,
+                                isTimestamp: true,
+                                claimingTime: symbioticVault.currentEpochStart()
+                                    + 2 * symbioticVault.epochDuration()
+                            });
+                        }
+                    }
+                }
+            } else if (subvault.protocol == IMultiVaultStorage.Protocol.EIGEN_LAYER) {
+                // TODO: add logic into the EigenLayerWithdrawalQueue
+                // IEigenLayerWithdrawalQueue q = IEigenLayerWithdrawalQueue(subvault.withdrawalQueue);
+                // q.getAccountData(account);
+                // AccountData storage accountData_ = _accountData[account];
+                // uint256[] memory indices = accountData_.withdrawals.values();
+                // uint256 block_ = latestWithdrawableBlock();
+                // uint256 counter = 0;
+                // uint256 shares = 0;
+                // for (uint256 i = 0; i < indices.length; i++) {
+                //     WithdrawalData storage withdrawal = _withdrawals[indices[i]];
+                //     if (withdrawal.isClaimed) {
+                //         continue;
+                //     } else if (block_ >= withdrawal.data.startBlock && counter < MAX_CLAIMING_WITHDRAWALS) {
+                //         counter++;
+                //     } else {
+                //         shares += withdrawal.sharesOf[account];
+                //     }
+                // }
+                // assets = shares == 0 ? 0 : IStrategy(strategy).sharesToUnderlyingView(shares);
+            } else {
+                revert("Invalid state");
+            }
+        }
+
+        assembly {
+            mstore(withdrawals, iterator)
+        }
+        r.withdrawals = withdrawals;
+    }
+
+    function collect(address user, address[] memory vaults)
+        public
+        view
+        returns (Response[] memory responses)
+    {
+        responses = new Response[](vaults.length);
+        for (uint256 i = 0; i < vaults.length; i++) {
+            responses[i] = collect(user, IERC4626(vaults[i]));
+        }
+    }
+
+    function multiCollect(address[] calldata users, address[] calldata vaults)
+        external
+        view
+        returns (Response[][] memory responses)
+    {
+        responses = new Response[][](users.length);
+        for (uint256 i = 0; i < users.length; i++) {
+            responses[i] = collect(users[i], vaults);
+        }
+    }
+
+    function fetchWithdrawalAmounts(uint256 lpAmount, address vault)
+        external
+        view
+        returns (uint256[] memory expectedAmounts, uint256[] memory expectedAmountsUSDC)
+    {
+        expectedAmounts = new uint256[](1);
+        expectedAmountsUSDC = new uint256[](1);
+        expectedAmounts[0] = IERC4626(vault).previewRedeem(lpAmount);
+        expectedAmountsUSDC[0] = oracle.getValue(IERC4626(vault).asset(), usd, expectedAmounts[0]);
+    }
+
+    function fetchDepositWrapperParams(address vault, address user, address token, uint256 amount)
+        external
+        view
+        returns (
+            bool isDepositPossible,
+            bool isDepositorWhitelisted,
+            bool isWhitelistedToken,
+            uint256 lpAmount,
+            uint256 depositValueUSDC
+        )
+    {
+        if (MultiVault(vault).depositPause()) {
+            return (false, false, false, 0, 0);
+        }
+        isDepositPossible = true;
+        if (MultiVault(vault).depositWhitelist() && !MultiVault(vault).isDepositorWhitelisted(user))
+        {
+            return (isDepositPossible, false, false, 0, 0);
+        }
+        isDepositorWhitelisted = true;
+
+        if (MultiVault(vault).asset() != wsteth) {
+            return (isDepositPossible, isDepositorWhitelisted, false, 0, 0);
+        }
+        if (token == weth || token == steth || token == wsteth || token == eth) {
+            isWhitelistedToken = true;
+        } else {
+            return (isDepositPossible, isDepositorWhitelisted, false, 0, 0);
+        }
+        if (token != wsteth) {
+            amount = IWSTETH(wsteth).getWstETHByStETH(amount);
+        }
+        lpAmount = MultiVault(vault).previewDeposit(amount);
+        depositValueUSDC = oracle.getValue(wsteth, usd, amount);
+    }
+
+    function fetchDepositAmounts(uint256[] memory amounts, address vault, address user)
+        external
+        view
+        returns (FetchDepositAmountsResponse memory r)
+    {
+        if (MultiVault(vault).depositPause()) {
+            return r;
+        }
+        r.isDepositPossible = true;
+        if (MultiVault(vault).depositWhitelist() && !MultiVault(vault).isDepositorWhitelisted(user))
+        {
+            return r;
+        }
+        r.isDepositorWhitelisted = true;
+        r.ratiosD18 = new uint256[](1);
+        r.ratiosD18[0] = D18;
+        r.tokens = new address[](1);
+        r.tokens[0] = MultiVault(vault).asset();
+        r.expectedLpAmount = MultiVault(vault).previewDeposit(amounts[0]);
+        r.expectedLpAmountUSDC = oracle.getValue(MultiVault(vault).asset(), usd, amounts[0]);
+        r.expectedAmounts = new uint256[](1);
+        r.expectedAmounts[0] = amounts[0];
+        r.expectedAmountsUSDC = new uint256[](1);
+        r.expectedAmountsUSDC[0] = r.expectedLpAmountUSDC;
+    }
+
+    function getWithdrawals(MultiVault vault, uint256 assets)
+        public
+        view
+        returns (Withdrawa[] memory withdrawals)
+    {
+        IWithdrawalStrategy strategy = vault.withdrawalStrategy();
+        IWithdrawalStrategy.WithdrawalData[] memory subvaultsData = strategy.calculateWithdrawalAmounts(address(vault), assets);
+        // take into account only pending and staked!
+        // withdrawals = new Wi
+        for (uint256 i = 0; i < subvaultsData.length; i++) {
+
+        } 
+    }
+
+    function getVaultAssets(
+        MultiVault vault,
+        address account,
+        uint256 shares // for withdrawal request
+    )
+        public
+        returns (
+            uint256 accountAssets,
+            uint256 accountInstantAssets,
+            Withdrawal[] memory withdrawals
+        )
+    {
+        address asset = vault.asset();
+        uint256 instantAssets = 0;
+        accountAssets = vault.previewRedeem(shares == 0 ? vault.balanceOf(account) : shares);
+        if (isDVV[address(vault)]) {
+            uint256 yieldVaultAssets = 0;
+            MultiVault yieldVault = DVV(address(vault)).yieldVault();
+            (instantAssets, yieldVaultAssets) = getVaultAssets(yieldVault, address(vault));
+            withdrawals = getWithdrawals(yieldVault, Math.min(yieldVaultAssets, userAssets));
+            instantAssets += IERC20(wsteth).balanceOf(address(vault));
+            instantAssets +=
+                IWSTETH(wsteth).getWstETHByStETH(IERC20(wsteth).balanceOf(address(vault)));
+        } else {
+            instantAssets = IERC20(asset).balanceOf(address(vault));
+            address collateral = vault.defaultCollateral();
+            if (collateral != address(0)) {
+                instantAssets += IERC20(collateral).balanceOf(address(vault));
+            }
+            uint256 subvaultsCount = vault.subvaultsCount();
+            for (uint256 i = 0; i < subvaultsCount; i++) {
+                IMultiVaultStorage.Subvault memory subvault = vault.subvaultAt(i);
+                if (subvault.withdrawalQueue == address(0)) {
+                    instantAssets += IERC4626(subvault.vault).previewRedeem(
+                        IERC4626(subvault.vault).balanceOf(address(vault))
+                    );
+                }
+            }
+            accountInstantAssets = Math.min(userAssets, instantAssets);
+            withdrawals = getWithdrawals(vault, userAssets);
+        }
+    }
+}
---
+++ scripts/holesky/collector/Oracle.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "./IOracle.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract Oracle {
+    uint256 public constant Q96 = 2 ** 96;
+
+    struct TokenOracle {
+        uint256 constValue;
+        address oracle;
+    }
+
+    address public owner;
+    mapping(address token => TokenOracle) public oracles;
+
+    constructor(address owner_) {
+        owner = owner_;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "EthOracle: not owner");
+        _;
+    }
+
+    function setOwner(address owner_) external onlyOwner {
+        owner = owner_;
+    }
+
+    function setOracles(address[] calldata tokens_, TokenOracle[] calldata oracles_)
+        external
+        onlyOwner
+    {
+        require(tokens_.length == oracles_.length, "EthOracle: invalid input");
+        for (uint256 i = 0; i < tokens_.length; i++) {
+            oracles[tokens_[i]] = oracles_[i];
+        }
+    }
+
+    /// @dev returns price in Q96 math in ETH for `token`
+    function priceX96(address token) public view returns (uint256) {
+        TokenOracle memory oracle = oracles[token];
+        if (oracle.constValue != 0) {
+            return oracle.constValue;
+        }
+        if (oracle.oracle == address(0)) {
+            revert("EthOracle: no oracle");
+        }
+        return IOracle(oracle.oracle).priceX96();
+    }
+
+    /// @dev returns price in Q96 math in `priceToken` for `token`
+    function priceX96(address token, address priceToken) public view returns (uint256) {
+        return Math.mulDiv(priceX96(token), Q96, priceX96(priceToken));
+    }
+
+    function getValue(address token, uint256 amount) public view returns (uint256) {
+        return Math.mulDiv(priceX96(token), amount, Q96);
+    }
+
+    function getValue(address token, address priceToken, uint256 amount)
+        public
+        view
+        returns (uint256)
+    {
+        return Math.mulDiv(priceX96(token, priceToken), amount, Q96);
+    }
+}
