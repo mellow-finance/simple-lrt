3497fe7cc948d6eca8ea9f1422fd4825d012b273
Anton Iants <yants.anton@gmail.com>
2025-01-17 15:20:18 +0400

    EigenLayer unit tests first part

--- test/BaseTest.sol
+++ test/BaseTest.sol
@@ -68,5 +68,84 @@ abstract contract BaseTest is Test {
         }
     }
 
+    function createDefaultMultiVaultWithEigenWstETHVault(address vaultAdmin)
+        internal
+        returns (
+            MultiVault vault,
+            EigenLayerAdapter eigenLayerAdapter,
+            RatiosStrategy strategy,
+            address eigenLayerVault
+        )
+    {
+        {
+            TransparentUpgradeableProxy c_ = new TransparentUpgradeableProxy(
+                address(new MultiVault("test", 1)), vm.createWallet("proxyAdmin").addr, new bytes(0)
+            );
+            vault = MultiVault(address(c_));
+        }
+        strategy = new RatiosStrategy();
+
+        IsolatedEigenLayerVaultFactory factory = new IsolatedEigenLayerVaultFactory(
+            Constants.HOLESKY_EL_DELEGATION_MANAGER,
+            address(new IsolatedEigenLayerWstETHVault(Constants.WSTETH())),
+            address(
+                new EigenLayerWstETHWithdrawalQueue(
+                    address(new Claimer()),
+                    Constants.HOLESKY_EL_DELEGATION_MANAGER,
+                    Constants.WSTETH()
+                )
+            ),
+            vm.createWallet("proxyAdmin").addr
+        );
+        eigenLayerAdapter = new EigenLayerWstETHAdapter(
+            address(factory),
+            address(vault),
+            IStrategyManager(Constants.HOLESKY_EL_STRATEGY_MANAGER),
+            IRewardsCoordinator(Constants.HOLESKY_EL_REWARDS_COORDINATOR),
+            Constants.WSTETH()
+        );
+
+        vault.initialize(
+            IMultiVault.InitParams({
+                admin: vaultAdmin,
+                limit: type(uint256).max,
+                depositPause: false,
+                withdrawalPause: false,
+                depositWhitelist: false,
+                asset: Constants.WSTETH(),
+                name: "MultiVault test",
+                symbol: "MVT",
+                depositStrategy: address(strategy),
+                withdrawalStrategy: address(strategy),
+                rebalanceStrategy: address(strategy),
+                defaultCollateral: address(0),
+                symbioticAdapter: address(0),
+                eigenLayerAdapter: address(eigenLayerAdapter),
+                erc4626Adapter: address(0)
+            })
+        );
+
+        {
+            ISignatureUtils.SignatureWithExpiry memory signature;
+            bytes32 salt = 0;
+            address operator = 0xbF8a8B0d0450c8812ADDf04E1BcB7BfBA0E82937; // random operator
+            address eigenLayerStrategy = 0x7D704507b76571a51d9caE8AdDAbBFd0ba0e63d3;
+            (eigenLayerVault,) = factory.getOrCreate(
+                address(vault), eigenLayerStrategy, operator, abi.encode(signature, salt)
+            );
+
+            vm.startPrank(vaultAdmin);
+            vault.grantRole(strategy.RATIOS_STRATEGY_SET_RATIOS_ROLE(), vaultAdmin);
+            vault.grantRole(vault.ADD_SUBVAULT_ROLE(), vaultAdmin);
+            vault.addSubvault(eigenLayerVault, IMultiVaultStorage.Protocol.EIGEN_LAYER);
+            address[] memory subvaults = new address[](1);
+            subvaults[0] = eigenLayerVault;
+            IRatiosStrategy.Ratio[] memory ratios = new IRatiosStrategy.Ratio[](1);
+            ratios[0] = IRatiosStrategy.Ratio(0.5 ether, 1 ether);
+            strategy.setRatios(address(vault), subvaults, ratios);
+            vm.stopPrank();
+        }
+    }
+
     function testBaseMock() private pure {}
 }
---
+++ test/unit/EigenLayerWithdrawalQueue.t.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "../BaseTest.sol";
+
+contract Unit is BaseTest {
+    using RandomLib for RandomLib.Storage;
+
+    function testConstructor() external {
+        address withdrawalQueue = address(
+            new EigenLayerWithdrawalQueue(
+                address(new Claimer()), Constants.HOLESKY_EL_DELEGATION_MANAGER
+            )
+        );
+
+        require(withdrawalQueue != address(0));
+    }
+
+    function testLatestWithdrawableBlock() external {
+        EigenLayerWithdrawalQueue withdrawalQueue = new EigenLayerWithdrawalQueue(
+            address(new Claimer()), Constants.HOLESKY_EL_DELEGATION_MANAGER
+        );
+        uint256 latestWithdrawableBlock = withdrawalQueue.latestWithdrawableBlock();
+
+        IStrategy[] memory strategies = new IStrategy[](1);
+        strategies[0] = IStrategy(withdrawalQueue.strategy());
+        require(
+            latestWithdrawableBlock
+                == block.number
+                    - IDelegationManager(withdrawalQueue.delegation()).getWithdrawalDelay(strategies)
+        );
+    }
+
+    function testInitialize() external {
+        Claimer claimer = new Claimer();
+
+        EigenLayerWithdrawalQueue withdrawalQueueSingleton =
+            new EigenLayerWithdrawalQueue(address(claimer), Constants.HOLESKY_EL_DELEGATION_MANAGER);
+
+        require(address(withdrawalQueueSingleton) != address(0));
+
+        vm.expectRevert(); // InvalidInitialization()
+        withdrawalQueueSingleton.initialize(address(1), address(2), address(3));
+
+        address proxyAdmin = rnd.randAddress();
+        new TransparentUpgradeableProxy{salt: bytes32(uint256(1))}(
+            address(withdrawalQueueSingleton),
+            proxyAdmin,
+            abi.encodeCall(
+                EigenLayerWithdrawalQueue.initialize, (address(1), address(2), address(3))
+            )
+        );
+    }
+
+    function testClaimableAssetsOf() external {
+        address vaultAdmin = rnd.randAddress();
+        (MultiVault vault,,,) = createDefaultMultiVaultWithEigenWstETHVault(vaultAdmin);
+        IEigenLayerWithdrawalQueue withdrawalQueue =
+            EigenLayerWstETHWithdrawalQueue(vault.subvaultAt(0).withdrawalQueue);
+
+        address user1 = rnd.randAddress();
+        address user2 = rnd.randAddress();
+
+        uint256 amount1 = 100 ether;
+        uint256 amount2 = 10 ether;
+
+        deal(Constants.WSTETH(), user1, amount1);
+        deal(Constants.WSTETH(), user2, amount2);
+
+        {
+            vm.startPrank(user1);
+            IERC20(Constants.WSTETH()).approve(address(vault), amount1);
+            vault.deposit(amount1, user1);
+            vault.withdraw(amount1 / 2, user1, user1);
+            vm.stopPrank();
+
+            assertEq(withdrawalQueue.claimableAssetsOf(user1), 0, "user1: claimableAssets");
+            vm.roll(block.number + 10); // skip delay
+            assertApproxEqAbs(
+                withdrawalQueue.claimableAssetsOf(user1), amount1 / 2, 2, "user1: claimableAssets"
+            );
+        }
+        {
+            vm.startPrank(user2);
+            IERC20(Constants.WSTETH()).approve(address(vault), amount2);
+            vault.deposit(amount2, user2);
+            vault.withdraw(amount2 / 2, user2, user2);
+            vm.stopPrank();
+
+            assertEq(withdrawalQueue.claimableAssetsOf(user2), 0, "user2: claimableAssets");
+            vm.roll(block.number + 10); // skip delay
+            assertApproxEqAbs(
+                withdrawalQueue.claimableAssetsOf(user2), amount2 / 2, 2, "user2: claimableAssets"
+            );
+        }
+
+        {
+            vm.startPrank(user1);
+            withdrawalQueue.claim(user1, user1, withdrawalQueue.claimableAssetsOf(user1));
+            vm.stopPrank();
+            assertEq(withdrawalQueue.claimableAssetsOf(user1), 0, "user1: claimableAssets");
+            assertApproxEqAbs(
+                IERC20(Constants.WSTETH()).balanceOf(user1), amount1 / 2, 3, "user1: balance"
+            );
+        }
+        {
+            vm.startPrank(user2);
+            withdrawalQueue.claim(user2, user2, withdrawalQueue.claimableAssetsOf(user2));
+            vm.stopPrank();
+            assertEq(withdrawalQueue.claimableAssetsOf(user2), 0, "user2: claimableAssets");
+            assertApproxEqAbs(
+                IERC20(Constants.WSTETH()).balanceOf(user2), amount2 / 2, 3, "user2: balance"
+            );
+        }
+
+        vm.startPrank(user1);
+        vault.withdraw(vault.maxWithdraw(user1), user1, user1);
+        vm.stopPrank();
+        assertEq(withdrawalQueue.claimableAssetsOf(user1), 0, "user1: claimableAssets");
+        vm.roll(block.number + 10); // skip delay
+        assertApproxEqAbs(
+            withdrawalQueue.claimableAssetsOf(user1), amount1 / 2, 3, "user1: claimableAssets"
+        );
+
+        vm.startPrank(user2);
+        vault.withdraw(vault.maxWithdraw(user2), user2, user2);
+        vm.stopPrank();
+        assertEq(withdrawalQueue.claimableAssetsOf(user2), 0, "user2: claimableAssets");
+        vm.roll(block.number + 10); // skip delay
+        assertApproxEqAbs(
+            withdrawalQueue.claimableAssetsOf(user2), amount2 / 2, 3, "user2: claimableAssets"
+        );
+
+        vm.startPrank(user1);
+        withdrawalQueue.claim(user1, user1, withdrawalQueue.claimableAssetsOf(user1));
+        vm.stopPrank();
+
+        assertApproxEqAbs(IERC20(Constants.WSTETH()).balanceOf(user1), amount1, 6, "user1: balance");
+
+        vm.startPrank(user2);
+        withdrawalQueue.claim(user2, user2, withdrawalQueue.claimableAssetsOf(user2));
+        vm.stopPrank();
+
+        assertApproxEqAbs(IERC20(Constants.WSTETH()).balanceOf(user2), amount2, 6, "user2: balance");
+
+        return;
+    }
+}
