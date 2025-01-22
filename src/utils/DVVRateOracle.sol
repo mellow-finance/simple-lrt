a33324f9bd05c9b5e49e50f7949ee974dd2e22a3
Andrey Borzenkov <armoking32@gmail.com>
2025-01-22 13:14:04 +0400

    DVVRateOracle: fix

--- src/utils/DVVRateOracle.sol
+++ src/utils/DVVRateOracle.sol
@@ -5,16 +5,19 @@ import "../interfaces/tokens/IWSTETH.sol";
 import "./VaultRateOracle.sol";
 
 contract DVVRateOracle is VaultRateOracle {
-    constructor(address vault_, address wsteth_) VaultRateOracle(vault_, wsteth_) {}
+    IWSTETH public WstETH;
+
+    constructor(address vault_, address wsteth_) VaultRateOracle(vault_, address(0)) {
+        WstETH = IWSTETH(wsteth_);
+    }
 
     function _getDeprecatedRate(uint256 shares) internal view override returns (uint256) {
         // flow for mellow-lrt@Vault
         (address[] memory tokens, uint256[] memory amounts) =
             IDeprecatedVault(vault).underlyingTvl();
         require(amounts.length == 2, "VaultRateOracle: invalid length");
-        uint256 wstethIndex = wsteth == tokens[0] ? 0 : 1;
-        uint256 wstethValue =
-            amounts[wstethIndex] + IWSTETH(wsteth).getWstETHByStETH(amounts[wstethIndex ^ 1]);
-        return Math.mulDiv(wstethValue, shares, IERC20(vault).totalSupply());
+        uint256 wstethIndex = address(WstETH) == tokens[0] ? 0 : 1;
+        uint256 wethValue = WstETH.getStETHByWstETH(amounts[wstethIndex]) + amounts[wstethIndex ^ 1];
+        return Math.mulDiv(wethValue, shares, IERC20(vault).totalSupply());
     }
 }
