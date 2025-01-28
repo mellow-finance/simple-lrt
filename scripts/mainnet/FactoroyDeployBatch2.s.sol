ee13dcca7d956b70a30d5c4707e3bfb2172fbf98
Andrey Borzenkov <armoking32@gmail.com>
2025-01-28 13:57:59 +0400

    params

--- scripts/mainnet/FactoroyDeployBatch2.s.sol
+++ scripts/mainnet/FactoroyDeployBatch2.s.sol
@@ -35,6 +35,8 @@ contract Deploy is Script {
     uint32 public constant VETO_SLASHER_INDEX = 1;
     uint32 public constant NETWORK_RESTAKE_DELEGATOR_INDEX = 0;
 
+    uint256 public constant RESOLVER_SET_EPOCHS_DELAY = 3;
+
     address public constant MELLOW_VAULT_FACTORY = 0x6EA5a344d116Db8949348648713760836D60fC5a;
 
     address public constant IBTC = 0x20157DBAbb84e3BBFE68C349d0d44E48AE7B5AD2;
@@ -51,6 +53,9 @@ contract Deploy is Script {
     address public constant IBTC_GLOBAL_RECEIVER = address(0xdead);
 
     uint256 public constant IBTC_VAULT_LIMIT = 235e8;
+
+    string public constant IBTC_VAULT_NAME = "Staked iBTC";
+    string public constant IBTC_VAULT_SYMBOL = "siBTC";
 
     function _createArray(address curator) internal pure returns (address[] memory a) {
         a = new address[](1);
@@ -117,7 +122,7 @@ contract Deploy is Script {
                     IVetoSlasher.InitParams({
                         baseParams: IBaseSlasher.BaseParams({isBurnerHook: true}),
                         vetoDuration: s.vetoDuration,
-                        resolverSetEpochsDelay: 3
+                        resolverSetEpochsDelay: RESOLVER_SET_EPOCHS_DELAY
                     })
                 )
             })
@@ -142,7 +147,9 @@ contract Deploy is Script {
         address asset = IBTC;
         uint256 vaultLimit = IBTC_VAULT_LIMIT;
         address globalReceiver = IBTC_GLOBAL_RECEIVER;
-        
+        string memory vaultName = IBTC_VAULT_NAME;
+        string memory vaultSymbol = IBTC_VAULT_SYMBOL;
+
         address defaultCollateral = IDefaultCollateralFactory(DEFAULT_COLLATERAL_FACTORY).create(
             asset, 0, vaultAdminMultisig
         );
@@ -157,8 +164,8 @@ contract Deploy is Script {
             depositPause: false,
             withdrawalPause: false,
             depositWhitelist: false,
-            name: "Staked iBTC",
-            symbol: "siBTC"
+            name: vaultName,
+            symbol: vaultSymbol
         });
 
         initParams.symbioticVault = _deploySymbioticVault(
