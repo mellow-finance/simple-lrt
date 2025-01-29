1f0a09f2f275e53b26cfa4f0315ca55ca6488d2f
Andrey Borzenkov <armoking32@gmail.com>
2025-01-29 05:56:55 +0400

    oracles

--- src/utils/Oracle.sol
+++ src/utils/Oracle.sol
@@ -4,20 +4,24 @@ pragma solidity 0.8.25;
 import "../interfaces/tokens/IWSTETH.sol";
 import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-contract VaultRateOracle {
+contract Oracle {
+    address public constant WstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
     address public immutable vault;
-    address public immutable wsteth;
+    bool public immutable isETHBased;
 
-    constructor(address vault_, address wsteth_) {
+    constructor(address vault_, bool isETHBased_) {
         vault = vault_;
-        wsteth = wsteth_;
+        if (isETHBased_) {
+            require(IERC4626(vault).asset() == WstETH, "Oracle: vault asset is not WstETH");
+        }
+        isETHBased = isETHBased_;
     }
 
     function convertToAssets(uint256 shares) public view returns (uint256 assets) {
         assets = IERC4626(vault).convertToAssets(shares);
-        if (wsteth != address(0)) {
+        if (isETHBased) {
             // we assume the price is in WSTETH token, so we want to convert it to ETH
-            assets = IWSTETH(wsteth).getStETHByWstETH(assets);
+            assets = IWSTETH(WstETH).getStETHByWstETH(assets);
         }
     }
 
