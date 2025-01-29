eff0297bd7d69b120053945ec3e3c435efffb346
Andrey Borzenkov <armoking32@gmail.com>
2025-01-29 19:28:46 +0400

    Migrator: WIP

--- src/utils/Migrator.sol
+++ src/utils/Migrator.sol
@@ -9,92 +9,98 @@ import "../adapters/SymbioticAdapter.sol";
 import "../vaults/MultiVault.sol";
 
 contract Migrator {
+    struct MigrationData {
+        address proxyAdminOwner;
+        address vault;
+        uint256 timestamp;
+    }
+
     address public immutable multiVaultSingleton;
     address public immutable strategy;
     address public immutable symbioticVaultFactory;
     address public immutable withdrawalQueueSingleton;
+    uint256 public immutable migrationDelay;
 
-    struct Migration {
-        address proxyAdmin;
-        address vaultAdmin;
-        address vaultProxyAdmin;
-        uint256 timestamp;
-    }
-
-    mapping(address vault => Migration) public migrations;
+    mapping(address proxyAdmin => MigrationData) public migrations;
 
     constructor(
         address multiVaultSingleton_,
         address strategy_,
         address symbioticVaultFactory_,
-        address withdrawalQueueSingleton_
+        address withdrawalQueueSingleton_,
+        uint256 migrationDelay_
     ) {
         multiVaultSingleton = multiVaultSingleton_;
         strategy = strategy_;
         symbioticVaultFactory = symbioticVaultFactory_;
         withdrawalQueueSingleton = withdrawalQueueSingleton_;
+        migrationDelay = migrationDelay_;
     }
 
-    function stageMigration(address vault, address proxyAdmin, address vaultAdmin) external {
-    
+    function stageMigration(ProxyAdmin proxyAdmin, address vault)
+        external
+        returns (MigrationData memory data)
+    {
         address proxyAdminOwner = proxyAdmin.owner();
-        if (proxyAdminOwners[vault] != address(0)) {
-            revert("Migrator: vault migration already staged");
-        }
         if (msg.sender != proxyAdminOwner) {
             revert("Migrator: sender not owner");
         }
-        proxyAdminOwners[vault] = proxyAdminOwner;
+        if (migrations[address(proxyAdmin)].timestamp != 0) {
+            revert("Migrator: vault migration already staged");
+        }
+        data = MigrationData({
+            proxyAdminOwner: proxyAdminOwner,
+            vault: vault,
+            timestamp: block.timestamp
+        });
+        migrations[address(proxyAdmin)] = data;
+        emit MigrationStaged(address(proxyAdmin), data);
     }
 
-    function cancelMigration(address vault) external {
-        address proxyAdminOwner = proxyAdminOwners[vault];
+    function cancelMigration(ProxyAdmin proxyAdmin) external {
+        address proxyAdminOwner = migrations[address(proxyAdmin)].proxyAdminOwner;
         if (msg.sender != proxyAdminOwner) {
             revert("Migrator: sender not owner");
         }
         if (proxyAdmin.owner() == address(this)) {
             proxyAdmin.transferOwnership(proxyAdminOwner);
         }
-        delete proxyAdminOwners[vault];
+        delete migrations[address(proxyAdmin)];
+        emit MigrationCancelled(address(proxyAdmin));
     }
 
-    function migrate(address vault, address vaultAdmin) external {
-        address proxyAdminOwner = proxyAdminOwners[vault];
-        if (proxyAdminOwner == address(0)) {
-            revert("Migrator: vault migration not staged");
-        }
-        if (proxyAdminOwner != msg.sender) {
+    function migrate(ProxyAdmin proxyAdmin) external {
+        MigrationData memory data = migrations[address(proxyAdmin)];
+        if (data.proxyAdminOwner != msg.sender) {
             revert("Migrator: sender not owner");
         }
-        ProxyAdmin proxyAdmin = getProxyAdmin(vault);
         if (proxyAdmin.owner() != address(this)) {
             revert("Migrator: ownership not transferred to migrator");
         }
-
-        address symbioticVault = IMellowSymbioticVault(vault).symbioticVault();
-
-        SymbioticAdapter symbioticAdapter = new SymbioticAdapter{salt: bytes32(bytes20(vault))}(
-            vault, symbioticVaultFactory, withdrawalQueueSingleton, proxyAdminOwner
-        );
+        IMellowSymbioticVault vault = IMellowSymbioticVault(data.vault);
+        address symbioticVault = vault.symbioticVault();
+        SymbioticAdapter symbioticAdapter = new SymbioticAdapter{
+            salt: bytes32(bytes20(address(vault)))
+        }(address(vault), symbioticVaultFactory, withdrawalQueueSingleton, data.proxyAdminOwner);
         proxyAdmin.upgradeAndCall(
-            ITransparentUpgradeableProxy(vault),
+            ITransparentUpgradeableProxy(address(vault)),
             multiVaultSingleton,
             abi.encodeCall(
                 MultiVault.initialize,
                 (
                     IMultiVault.InitParams({
                         admin: address(this),
-                        limit: IMellowSymbioticVault(vault).limit(),
-                        depositPause: IMellowSymbioticVault(vault).depositPause(),
-                        withdrawalPause: IMellowSymbioticVault(vault).withdrawalPause(),
-                        depositWhitelist: IMellowSymbioticVault(vault).depositWhitelist(),
-                        asset: IMellowSymbioticVault(vault).asset(),
-                        name: IMellowSymbioticVault(vault).name(),
-                        symbol: IMellowSymbioticVault(vault).symbol(),
+                        limit: vault.limit(),
+                        depositPause: vault.depositPause(),
+                        withdrawalPause: vault.withdrawalPause(),
+                        depositWhitelist: vault.depositWhitelist(),
+                        asset: vault.asset(),
+                        name: vault.name(),
+                        symbol: vault.symbol(),
                         depositStrategy: address(strategy),
                         withdrawalStrategy: address(strategy),
                         rebalanceStrategy: address(strategy),
-                        defaultCollateral: IMellowSymbioticVault(vault).defaultCollateral(),
+                        defaultCollateral: vault.defaultCollateral(),
                         symbioticAdapter: address(symbioticAdapter),
                         eigenLayerAdapter: address(0),
                         erc4626Adapter: address(0)
@@ -103,11 +109,17 @@ contract Migrator {
             )
         );
 
-        MultiVault multiVault = MultiVault(vault);
+        MultiVault multiVault = MultiVault(address(vault));
         multiVault.grantRole(multiVault.ADD_SUBVAULT_ROLE(), address(this));
         multiVault.addSubvault(symbioticVault, IMultiVaultStorage.Protocol.SYMBIOTIC);
-        multiVault.grantRole(multiVault.DEFAULT_ADMIN_ROLE(), vaultAdmin);
         multiVault.renounceRole(multiVault.ADD_SUBVAULT_ROLE(), address(this));
         multiVault.renounceRole(multiVault.DEFAULT_ADMIN_ROLE(), address(this));
+        delete migrations[address(proxyAdmin)];
+
+        emit MigrationExecuted(address(proxyAdmin), data);
     }
+
+    event MigrationStaged(address indexed proxyAdmin, MigrationData data);
+    event MigrationCancelled(address indexed proxyAdmin);
+    event MigrationExecuted(address indexed proxyAdmin, MigrationData data);
 }
