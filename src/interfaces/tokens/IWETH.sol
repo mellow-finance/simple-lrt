e2f83a87819f1d6b0bdeaa7ae0deb130005d5b5f
Andrey Borzenkov <armoking32@gmail.com>
2025-04-11 12:58:12 +0400

    fix

--- src/interfaces/tokens/IWETH.sol
+++ src/interfaces/tokens/IWETH.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.25;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IWETH is IERC20 {
+interface IWETH {
     function deposit() external payable;
     function withdraw(uint256 _wad) external;
 }
--- src/interfaces/utils/IStakingModule.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.25;
-
-interface IStakingModule {
-    function stake(bytes calldata data, address caller) external;
-
-    function forceStake(uint256 amount) external;
-}
