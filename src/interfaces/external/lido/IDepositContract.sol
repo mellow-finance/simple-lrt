e3789a970e651b050af8ca13eaad274733d3e218
Andrey Borzenkov <armoking32@gmail.com>
2025-04-11 12:58:39 +0400

    fix

--- src/interfaces/external/lido/IDepositContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity 0.8.25;
-
-interface IDepositContract {
-    function get_deposit_root() external view returns (bytes32 rootHash);
-}
