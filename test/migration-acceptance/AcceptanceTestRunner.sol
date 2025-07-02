57490319cdfb471c115f88bd2e002664b0137644
Andrey Borzenkov <armoking32@gmail.com>
2025-07-03 00:43:11 +0400

    simple-lrt -> multi-vault migration tests

---
+++ test/migration-acceptance/AcceptanceTestRunner.sol
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "../../scripts/deploy/DeployScript.sol";
+import "../../scripts/deploy/libraries/EigenLayerDeployLibrary.sol";
+import "../../scripts/deploy/libraries/SymbioticDeployLibrary.sol";
+import "../../src/utils/Claimer.sol";
+
+import "@openzeppelin/contracts/access/extensions/IAccessControlEnumerable.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "@symbiotic/core/interfaces/IDelegatorFactory.sol";
+import "@symbiotic/core/interfaces/ISlasherFactory.sol";
+import "@symbiotic/core/interfaces/vault/IVaultStorage.sol";
+
+import "forge-std/StdAssertions.sol";
+import "forge-std/console2.sol";
+
+import "../../src/utils/Migrator.sol";
+
+/*
+    1. какие-то пользователи будут с pending or claimable assets - нужно проверить частично
+    2. totalSupply, tvl symbiotic deposits, default collateral не должны поменяться
+    3. пользователи, стейт системы, коил
+*/
+interface IMellowSymbioticVaultQueue {
+    function withdrawalQueue() external view returns (address);
+}
+
+abstract contract AcceptanceTestRunner is StdAssertions {
+    struct PairAddressBytes32 {
+        address address_;
+        bytes32 bytes32_;
+    }
+
+    // AccessControl roles:
+    bytes32 private DEFAULT_ADMIN_ROLE = 0x00;
+
+    // VaultControl roles
+    bytes32 private constant SET_LIMIT_ROLE = keccak256("SET_LIMIT_ROLE");
+    bytes32 private constant PAUSE_WITHDRAWALS_ROLE = keccak256("PAUSE_WITHDRAWALS_ROLE");
+    bytes32 private constant UNPAUSE_WITHDRAWALS_ROLE = keccak256("UNPAUSE_WITHDRAWALS_ROLE");
+    bytes32 private constant PAUSE_DEPOSITS_ROLE = keccak256("PAUSE_DEPOSITS_ROLE");
+    bytes32 private constant UNPAUSE_DEPOSITS_ROLE = keccak256("UNPAUSE_DEPOSITS_ROLE");
+    bytes32 private constant SET_DEPOSIT_WHITELIST_ROLE = keccak256("SET_DEPOSIT_WHITELIST_ROLE");
+    bytes32 private constant SET_DEPOSITOR_WHITELIST_STATUS_ROLE =
+        keccak256("SET_DEPOSITOR_WHITELIST_STATUS_ROLE");
+
+    // MultiVault roles
+    bytes32 private constant ADD_SUBVAULT_ROLE = keccak256("ADD_SUBVAULT_ROLE");
+    bytes32 private constant REMOVE_SUBVAULT_ROLE = keccak256("REMOVE_SUBVAULT_ROLE");
+    bytes32 private constant SET_STRATEGY_ROLE = keccak256("SET_STRATEGY_ROLE");
+    bytes32 private constant SET_FARM_ROLE = keccak256("SET_FARM_ROLE");
+    bytes32 private constant REBALANCE_ROLE = keccak256("REBALANCE_ROLE");
+    bytes32 private constant SET_DEFAULT_COLLATERAL_ROLE = keccak256("SET_DEFAULT_COLLATERAL_ROLE");
+    bytes32 private constant SET_ADAPTER_ROLE = keccak256("SET_ADAPTER_ROLE");
+
+    // RatiosStrategy roles:
+    bytes32 private constant RATIOS_STRATEGY_SET_RATIOS_ROLE =
+        keccak256("RATIOS_STRATEGY_SET_RATIOS_ROLE");
+
+    bytes32[16] internal roles = [
+        DEFAULT_ADMIN_ROLE,
+        SET_LIMIT_ROLE,
+        PAUSE_WITHDRAWALS_ROLE,
+        UNPAUSE_WITHDRAWALS_ROLE,
+        PAUSE_DEPOSITS_ROLE,
+        UNPAUSE_DEPOSITS_ROLE,
+        SET_DEPOSIT_WHITELIST_ROLE,
+        SET_DEPOSITOR_WHITELIST_STATUS_ROLE,
+        ADD_SUBVAULT_ROLE,
+        REMOVE_SUBVAULT_ROLE,
+        SET_STRATEGY_ROLE,
+        SET_FARM_ROLE,
+        REBALANCE_ROLE,
+        SET_DEFAULT_COLLATERAL_ROLE,
+        SET_ADAPTER_ROLE,
+        RATIOS_STRATEGY_SET_RATIOS_ROLE
+    ];
+
+    Migrator internal immutable migrator = Migrator(0x37BE38A8Bd5D84DeFA072fFf6c0E1d923e9563EB);
+    address internal constant VAULT_ADMIN = 0x9437B2a8cF3b69D782a61f9814baAbc172f72003;
+    address internal constant VAULT_PROXY_ADMIN = 0x81698f87C6482bF1ce9bFcfC0F103C4A0Adf0Af0;
+    address internal constant STRATEGY = 0x3aA61E6196fb3eb1170E578ad924898624f54ad6;
+    address internal constant WSTETH_DEFAULT_COLLATERAL = 0xC329400492c6ff2438472D4651Ad17389fCb843a;
+    address internal constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+
+    struct State {
+        address symbioticVault;
+        PairAddressBytes32[] permissions;
+        uint256 limit;
+        uint256 totalSupply;
+        uint256 totalAssets;
+        address symbioticWithdrawalQueue;
+        address defaultCollateral;
+        address asset;
+    }
+
+    function getCommonState(address vault) public view returns (State memory $) {
+        MultiVault vault_ = MultiVault(vault);
+        $.limit = vault_.limit();
+        $.totalSupply = vault_.totalSupply();
+        $.totalAssets = vault_.totalAssets();
+        $.asset = vault_.asset();
+
+        uint256 count = 0;
+        for (uint256 i = 0; i < roles.length; i++) {
+            count += vault_.getRoleMemberCount(roles[i]);
+        }
+        $.permissions = new PairAddressBytes32[](count);
+        uint256 index = 0;
+        for (uint256 i = 0; i < roles.length; i++) {
+            bytes32 role = roles[i];
+            uint256 roleCount = vault_.getRoleMemberCount(role);
+            for (uint256 j = 0; j < roleCount; j++) {
+                $.permissions[index] =
+                    PairAddressBytes32({address_: vault_.getRoleMember(role, j), bytes32_: role});
+                index++;
+            }
+        }
+    }
+
+    function loadSimpleLRTState(address vault) public view returns (State memory $) {
+        $ = getCommonState(vault);
+        $.symbioticVault = IMellowSymbioticVault(vault).symbioticVault();
+        $.symbioticWithdrawalQueue = IMellowSymbioticVaultQueue(vault).withdrawalQueue();
+        $.defaultCollateral = address(IMellowSymbioticVault(vault).symbioticCollateral());
+    }
+
+    function loadMultiVaultState(address vault) public view returns (State memory $) {
+        $ = getCommonState(vault);
+        MultiVault vault_ = MultiVault(vault);
+        $.symbioticVault = vault_.subvaultAt(0).vault;
+        $.symbioticWithdrawalQueue = vault_.subvaultAt(0).withdrawalQueue;
+        $.defaultCollateral = address(vault_.defaultCollateral());
+    }
+
+    function getProxyAdmin(address proxyAddress) public view returns (address) {
+        bytes memory proxyCode = address(proxyAddress).code;
+        require(proxyCode.length >= 28 + 20, "getProxyAdmin: invalid proxy code length");
+        address proxyAdmin;
+        assembly {
+            proxyAdmin := mload(add(proxyCode, 48))
+        }
+        return proxyAdmin;
+    }
+
+    function getProxyAdminOwner(address proxyAddress) public view returns (address) {
+        address proxyAdmin = getProxyAdmin(proxyAddress);
+        return ProxyAdmin(proxyAdmin).owner();
+    }
+
+    function validateState(address vaultAddress, State memory before_, State memory after_)
+        internal
+        view
+    {
+        assertNotEq(vaultAddress, address(0), "validateState: vault address is zero");
+        assertTrue(migrator.isEntity(vaultAddress), "validateState: vault is not a migrated entity");
+        assertEq(
+            getProxyAdminOwner(vaultAddress),
+            VAULT_PROXY_ADMIN,
+            "validateState: proxy admin owner is not correct"
+        );
+        MultiVault vault = MultiVault(vaultAddress);
+        assertTrue(
+            vault.hasRole(DEFAULT_ADMIN_ROLE, VAULT_ADMIN),
+            "validateState: vault admin role is not set correctly"
+        );
+        assertEq(before_.asset, after_.asset, "Asset should not change");
+        assertEq(before_.totalSupply, after_.totalSupply, "Total supply should not change");
+        assertEq(before_.totalAssets, after_.totalAssets, "Total assets should not change");
+        assertEq(
+            before_.defaultCollateral,
+            after_.defaultCollateral,
+            "Default collateral should not change"
+        );
+        assertEq(before_.symbioticVault, after_.symbioticVault, "Symbiotic vault should not change");
+        assertNotEq(
+            before_.symbioticWithdrawalQueue,
+            after_.symbioticWithdrawalQueue,
+            "Symbiotic withdrawal queue **should** change"
+        );
+        assertEq(before_.limit, after_.limit, "Limit should not change");
+        assertEq(
+            before_.permissions.length,
+            after_.permissions.length,
+            "Permissions length should not change"
+        );
+
+        for (uint256 i = 0; i < before_.permissions.length; i++) {
+            assertEq(
+                before_.permissions[i].address_,
+                after_.permissions[i].address_,
+                "Permission address should not change"
+            );
+            assertEq(
+                before_.permissions[i].bytes32_,
+                after_.permissions[i].bytes32_,
+                "Permission role should not change"
+            );
+        }
+
+        assertEq(vault.asset(), WSTETH, "Asset should be WSTETH");
+        assertEq(
+            address(vault.defaultCollateral()),
+            WSTETH_DEFAULT_COLLATERAL,
+            "Default collateral should be WSTETH_DEFAULT_COLLATERAL"
+        );
+        assertEq(vault.subvaultsCount(), 1, "MultiVault should have exactly one subvault");
+        assertNotEq(
+            address(vault.symbioticAdapter()),
+            address(0),
+            "MultiVault should have a valid symbiotic adapter"
+        );
+        assertEq(
+            address(vault.eigenLayerAdapter()),
+            address(0),
+            "MultiVault should not have an EigenLayer adapter"
+        );
+        assertEq(
+            address(vault.erc4626Adapter()),
+            address(0),
+            "MultiVault should not have an ERC4626 adapter"
+        );
+        assertEq(
+            uint256(vault.subvaultAt(0).protocol),
+            uint256(0),
+            "Subvault protocol should be set to 0 (Symbiotic)"
+        );
+    }
+}
---
+++ test/migration-acceptance/Test.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "./AcceptanceTestRunner.sol";
+import "forge-std/Test.sol";
+
+contract Acceptance is AcceptanceTestRunner, Test {
+    function migrateAndCheck(address vault) public {
+        address proxyAdmin = getProxyAdmin(vault);
+
+        (,, uint256 stageTimestamp) = migrator.migrations(proxyAdmin);
+        vm.startPrank(VAULT_PROXY_ADMIN);
+        if (stageTimestamp == 0 && !migrator.isEntity(vault)) {
+            migrator.stageMigration(ProxyAdmin(proxyAdmin), vault);
+            skip(4 hours);
+            ProxyAdmin(proxyAdmin).transferOwnership(address(migrator));
+            State memory before_ = loadSimpleLRTState(vault);
+            migrator.executeMigration(ProxyAdmin(proxyAdmin), VAULT_ADMIN);
+            State memory after_ = loadMultiVaultState(vault);
+            validateState(vault, before_, after_);
+        } else if (stageTimestamp != 0 && !migrator.isEntity(vault)) {
+            skip(stageTimestamp - block.timestamp);
+            ProxyAdmin(proxyAdmin).transferOwnership(address(migrator));
+            State memory before_ = loadSimpleLRTState(vault);
+            migrator.executeMigration(ProxyAdmin(proxyAdmin), VAULT_ADMIN);
+            State memory after_ = loadMultiVaultState(vault);
+            validateState(vault, before_, after_);
+        } else if (stageTimestamp == 0 && migrator.isEntity(vault)) {
+            State memory state_ = loadMultiVaultState(vault);
+            validateState(vault, state_, state_);
+        } else {
+            revert("Invalid migrator state");
+        }
+        vm.stopPrank();
+
+        runUserFlow(vault);
+    }
+
+    function runUserFlow(address vault) public {
+        MultiVault v = MultiVault(vault);
+        for (uint256 i = 1; i <= 10; i++) {
+            address user = vm.createWallet(string.concat("random-user-", vm.toString(i))).addr;
+            vm.startPrank(user);
+            uint256 amount = i * 1 ether;
+            vm.deal(user, amount);
+            Address.sendValue(payable(WSTETH), amount);
+            uint256 balance = IERC20(WSTETH).balanceOf(user);
+            IERC20(WSTETH).approve(address(v), balance);
+            uint256 shares = v.deposit(balance, user);
+
+            assertEq(v.balanceOf(user), shares, "User shares mismatch");
+
+            v.redeem(shares, user, user);
+            assertEq(v.balanceOf(user), 0, "User shares not redeemed");
+
+            // due to 0 min/max ratio set for symbiotic subvault all such withdrawals will be instant
+            assertApproxEqAbs(
+                IERC20(WSTETH).balanceOf(user),
+                balance,
+                1 wei,
+                "User WSTETH balance mismatch after redeem"
+            );
+
+            vm.stopPrank();
+        }
+    }
+
+    function testMultiMigration() external {
+        address[14] memory vaults = [
+            0xB908c9FE885369643adB5FBA4407d52bD726c72d,
+            0x24183535a24CF0272841B05047A26e200fFAB696,
+            0xBEEF69Ac7870777598A04B2bd4771c71212E6aBc,
+            0x84631c0d0081FDe56DeB72F6DE77abBbF6A9f93a,
+            0x5fD13359Ba15A84B76f7F87568309040176167cd,
+            0x8c9532a60E0E7C6BbD2B2c1303F63aCE1c3E9811,
+            0x7a4EffD87C2f3C55CA251080b1343b605f327E3a,
+            0xcC36e5272c422BEE9A8144cD2493Ac472082eBaD,
+            0x4f3Cc6359364004b245ad5bE36E6ad4e805dC961,
+            0x49cd586dd9BA227Be9654C735A659a1dB08232a9,
+            0x82dc3260f599f4fC4307209A1122B6eAa007163b,
+            0x375A8eE22280076610cA2B4348d37cB1bEEBeba0,
+            0xd6E09a5e6D719d1c881579C9C8670a210437931b,
+            0x7b31F008c48EFb65da78eA0f255EE424af855249
+        ];
+
+        for (uint256 i = 0; i < vaults.length; i++) {
+            migrateAndCheck(vaults[i]);
+        }
+    }
+}
