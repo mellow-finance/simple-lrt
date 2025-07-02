c14773aedadde44f9595064d80cb39e04fa3342d
Andrey Borzenkov <armoking32@gmail.com>
2025-07-02 16:09:16 +0400

    fixes

--- test/unit/DVVTest.t.sol
+++ test/unit/DVVTest.t.sol
@@ -13,8 +13,7 @@ contract Unit is Test {
     address public immutable wsteth = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
 
     function testDVVMigration() external {
-        DVV dvvSingleton = new DVV();
-        MigratorDVV migratorDVV = new MigratorDVV(address(dvvSingleton), 11679 ether);
+        MigratorDVV migratorDVV = MigratorDVV(0x0000003cE4397E957a5634f1402D29Ca5D239319);
         // tx 1
         vm.startPrank(migratorDVV.ADMIN());
         IAccessControl(migratorDVV.SIMPLE_DVT_STAKING_STRATEGY()).grantRole(
